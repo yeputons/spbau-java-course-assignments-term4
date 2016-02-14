6ed9c634f85655fcbeffc53cb3465bf58ccdcdaf
Denis Zharkov <denis.zharkov@jetbrains.com>
2016-02-14 17:55:03 +0300

    Create stub project structure

---
+++ src/main/java/ru/spbau/mit/Main.java
@@ -0,0 +1,6 @@
+package ru.spbau.mit;
+public class Main {
+    public static void main(String[] args) {
+        throw new RuntimeException();
+    }
+}
---
+++ src/test/java/ru/spbau/mit/StubTest.java
@@ -0,0 +1,10 @@
+package ru.spbau.mit;
+
+import org.junit.Test;
+
+public class StubTest {
+    @Test
+    public void testSettings() {
+        Main.main(null);
+    }
+}
