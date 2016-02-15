c7bbb43c996623b6c26bdd3eb189fa3b8f82b1e5
Semyon Proshev <s-proshev@ya.ru>
2016-02-15 23:29:21 +0300

    add second part tasks to the task about stream

---
+++ src/test/java/ru/spbau/mit/SecondPartTasksTest.java
@@ -0,0 +1,28 @@
+package ru.spbau.mit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class SecondPartTasksTest {
+
+    @Test
+    public void testFindQuotes() {
+        fail();
+    }
+
+    @Test
+    public void testPiDividedBy4() {
+        fail();
+    }
+
+    @Test
+    public void testFindPrinter() {
+        fail();
+    }
+
+    @Test
+    public void testCalculateGlobalOrder() {
+        fail();
+    }
+}
