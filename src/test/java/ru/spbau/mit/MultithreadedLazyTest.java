9f089406b2c317dbf88af6c5b34b1c4fc740bd34
Egor Suvorov <egor.suvorov@gmail.com>
2016-02-22 13:55:07 +0300

    MultithreadedLazyTest: code cleanup: replace variable+loop with streams

--- src/test/java/ru/spbau/mit/MultithreadedLazyTest.java
+++ src/test/java/ru/spbau/mit/MultithreadedLazyTest.java
@@ -76,16 +76,12 @@ public class MultithreadedLazyTest {
         }
         Integer[][] results = runThreads(suppliers);
 
-        boolean haveNull = false;
         for (int lazy = 0; lazy < LAZIES_COUNT; lazy++) {
             for (int th = 0; th < THREADS_COUNT; th++) {
                 assertEquals(results[0][lazy], results[th][lazy]);
             }
-            if (results[0][lazy] == null) {
-                haveNull = true;
-            }
         }
-        assertEquals(shouldHaveNull, haveNull);
+        assertEquals(shouldHaveNull, Arrays.stream(results[0]).anyMatch(r -> r == null));
 
         for (TestSupplier s : suppliers) {
             if (shouldComputeExactlyOnce) {
