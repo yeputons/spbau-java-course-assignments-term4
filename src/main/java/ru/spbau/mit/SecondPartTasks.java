916ca55e7854ceafe7636bd0b472d3e9694d13bd
Egor Suvorov <egor.suvorov@gmail.com>
2016-02-22 19:46:07 +0300

    SecondPartTasks: fix checkstyle

--- src/main/java/ru/spbau/mit/SecondPartTasks.java
+++ src/main/java/ru/spbau/mit/SecondPartTasks.java
@@ -1,11 +1,8 @@
 package ru.spbau.mit;
 
 import java.awt.geom.Point2D;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
