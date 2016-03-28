600aeb466b1d164fbfac12321649e2321c53f157
Egor Suvorov <egor.suvorov@gmail.com>
2016-03-28 20:51:47 +0300

    Refactor SimpleFtpClientHandler: make use of try-with-resources for DataStreams

--- src/main/java/ru/spbau/mit/SimpleFtpClientHandler.java
+++ src/main/java/ru/spbau/mit/SimpleFtpClientHandler.java
@@ -16,9 +16,6 @@ class SimpleFtpClientHandler implements Runnable {
     private final Path rootPath;
     private final Socket client;
 
-    private DataInputStream in;
-    private DataOutputStream out;
-
     SimpleFtpClientHandler(Path rootPath, Socket client) {
         this.rootPath = rootPath;
         this.client = client;
@@ -26,40 +23,15 @@ class SimpleFtpClientHandler implements Runnable {
 
     @Override
     public void run() {
-        try {
-            in = new DataInputStream(client.getInputStream());
-            out = new DataOutputStream(client.getOutputStream());
-            while (true) {
-                int command = in.readInt();
-                switch (command) {
-                    case COMMAND_LIST:
-                        handleList();
-                        break;
-                    case COMMAND_GET:
-                        handleGet();
-                        break;
-                    default:
-                        throw new RuntimeException("Invalid command received from client");
-                }
-            }
+        try (
+            DataInputStream in = new DataInputStream(client.getInputStream());
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+        ) {
+            new Handler(in, out).run();
         } catch (EOFException e) {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                if (out != null) {
-                    out.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
             if (!client.isClosed()) {
                 try {
                     client.shutdownInput();
@@ -80,34 +52,60 @@ class SimpleFtpClientHandler implements Runnable {
         }
     }
 
-    private void handleList() throws IOException {
-        Path dir = rootPath.resolve(in.readUTF());
-        if (!Files.isDirectory(dir)) {
-            out.writeInt(0);
-            return;
-        }
-        List<Path> content =
-                Files.list(dir)
-                        .sorted(Comparator.comparing(Path::getFileName))
-                        .collect(Collectors.toList());
-        out.writeInt(content.size());
-        for (Path inside : content) {
-            out.writeUTF(inside.getFileName().toString());
-            out.writeBoolean(Files.isDirectory(inside));
-        }
-    }
+    private final class Handler {
+        private final DataInputStream in;
+        private final DataOutputStream out;
 
-    private void handleGet() throws IOException {
-        Path file = rootPath.resolve(in.readUTF());
-        if (Files.isDirectory(file)) {
-            out.writeInt(0);
-            return;
+        private Handler(DataInputStream in, DataOutputStream out) {
+            this.in = in;
+            this.out = out;
         }
-        try (InputStream fin = Files.newInputStream(file)) {
-            out.writeInt((int) Files.size(file));
-            IOUtils.copyLarge(fin, out);
-        } catch (NoSuchFileException | UnsupportedOperationException | AccessDeniedException e) {
-            out.writeInt(0);
+
+        public void run() throws IOException {
+            while (true) {
+                int command = in.readInt();
+                switch (command) {
+                    case COMMAND_LIST:
+                        handleList();
+                        break;
+                    case COMMAND_GET:
+                        handleGet();
+                        break;
+                    default:
+                        throw new RuntimeException("Invalid command received from client");
+                }
+            }
+        }
+
+        private void handleList() throws IOException {
+            Path dir = rootPath.resolve(in.readUTF());
+            if (!Files.isDirectory(dir)) {
+                out.writeInt(0);
+                return;
+            }
+            List<Path> content =
+                    Files.list(dir)
+                            .sorted(Comparator.comparing(Path::getFileName))
+                            .collect(Collectors.toList());
+            out.writeInt(content.size());
+            for (Path inside : content) {
+                out.writeUTF(inside.getFileName().toString());
+                out.writeBoolean(Files.isDirectory(inside));
+            }
+        }
+
+        private void handleGet() throws IOException {
+            Path file = rootPath.resolve(in.readUTF());
+            if (Files.isDirectory(file)) {
+                out.writeInt(0);
+                return;
+            }
+            try (InputStream fin = Files.newInputStream(file)) {
+                out.writeInt((int) Files.size(file));
+                IOUtils.copyLarge(fin, out);
+            } catch (NoSuchFileException | UnsupportedOperationException | AccessDeniedException e) {
+                out.writeInt(0);
+            }
         }
     }
 }
